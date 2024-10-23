fcf29f7f94f3a0b30cf86f12a09daae81addac68
Guillaume Ste-Marie <94798913+Guiss-Guiss@users.noreply.github.com>
2024-10-22 20:08:33 -0400

    Add files via upload

--- rag_component.py
+++ rag_component.py
@@ -4,40 +4,55 @@ import asyncio
 from concurrent.futures import ThreadPoolExecutor
 from language_utils import get_translation, get_current_language
 from langdetect import detect
+from functools import partial
 
 logger = logging.getLogger(__name__)
 
 class RAGComponent:
     def __init__(self, model_name="llama3.1:latest"):
         self.model_name = model_name
-        self.executor = ThreadPoolExecutor(max_workers=1)
+        self._executor = None
         logger.info(get_translation("rag_component_initialized").format(model=self.model_name))
 
-    def _generate_answer_sync(self, query, context_chunks):
-        lang = detect(query)
+    @property
+    def executor(self):
+        if self._executor is None:
+            self._executor = ThreadPoolExecutor(max_workers=2, thread_name_prefix="RAG_Worker")
+        return self._executor
+
+    async def _generate_answer(self, query, context_chunks):
         try:
+            lang = detect(query)
             logger.info(get_translation("generating_answer_for_query").format(query=query))
-            context = "\n".join([chunk['content'] for chunk in context_chunks])
+            
+            context = "\n".join(chunk['content'] for chunk in context_chunks)
             prompt = get_translation("rag_prompt").format(context=context, query=query)
 
-            response = ollama.chat(model=self.model_name, messages=[
-                {
-                    'role': 'user',
-                    'content': prompt
-                }
-            ])
+            response = await asyncio.to_thread(
+                lambda: ollama.chat(
+                    model=self.model_name,
+                    messages=[{'role': 'user', 'content': prompt}]
+                )
+            )
 
             if response and 'message' in response:
                 answer = response['message']['content']
                 logger.info(get_translation("answer_generated_successfully"))
                 return answer
-            else:
-                logger.warning(get_translation("no_valid_response_from_model"))
-                return get_translation("couldnt_generate_answer")
+
+            logger.warning(get_translation("no_valid_response_from_model"))
+            return get_translation("couldnt_generate_answer")
+
         except Exception as e:
-            logger.error(get_translation("error_generating_answer").format(error=str(e)), exc_info=True)
+            logger.error(
+                get_translation("error_generating_answer").format(error=str(e)), 
+                exc_info=True
+            )
             return get_translation("error_occurred_while_generating").format(error=str(e))
 
     async def generate_answer(self, query, context_chunks):
-        loop = asyncio.get_running_loop()
-        return await loop.run_in_executor(self.executor, self._generate_answer_sync, query, context_chunks)
+        return await self._generate_answer(query, context_chunks)
+
+    def __del__(self):
+        if self._executor:
+            self._executor.shutdown(wait=False)
